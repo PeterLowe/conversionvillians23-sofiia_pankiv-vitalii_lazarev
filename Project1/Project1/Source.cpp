331a986a37743991764037f001a7d03fb0f56a7d
(Student  C00299313) Vitalii Lazarev <C00299313@setu.ie>
2023-11-17 10:51:34 +0000

    Added code for temperature

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -2,42 +2,45 @@
 
 #include <iostream>
 
-<<<<<<< HEAD
+
 char printMenu() // written by Sofiia
 {
+    float t_celcius = 0;
     char choice = ' ';// user input  choice
     std::cout << "Please enter a upper case letter to convert from left to right\nand lower to convert from right to left\n\n" << std::endl;
     std::cout << "[A] Degrees---<--->---Radians        [a]" << std::endl;
     std::cout << "[B] Centimeters-<--->--Feet & Inches  [b]" << std::endl;
     std::cout << "[C] Litres---<--->---Gallons        [c]" << std::endl;
-
+	std::cout << "[D] Mpg <---> L/100km [d]" << std::endl;
+	std::cout << "[E] Kilograms <---> Stone & Pounds [e]" << std::endl;
+	std::cout << "[F] Celcius <---> Fahrenheit [f]" << std::endl;
     std::cout << "[Q] Exit        [---]   Exit           [q]" << std::endl;
     std::cout << "Enter your Choice : ";
     std::cin >> choice;
+    char userChoice; // letter for user to pick conversion
+    userChoice = printMenu(); // only print the menu once
+    if (userChoice == 'f')
+    {
+        getTemperature(t_celcius);
+    }
     return choice;
 }
 
-=======
->>>>>>> master
+float getTemperature(float t_celcius)
+{
+    float t_fahrenheit = 0;
+    std::cout << "Enter temperature in fahrenheit: " << std::endl;
+    std::cin >> t_fahrenheit;
+    t_celcius = (t_fahrenheit - 32) * 5.0 / 9.0;
+    std::cout << "Celcius is: " << t_celcius << std::endl;
+    return t_celcius;
+}
+
+
 int main() {
     char choice;
     choice = printMenu();
 
 
 	return 1;
-}
-
-// Vitalii Lazarev
-
-char printMenu() // written by Vitalii Lazarev
-{
-	char choice = ' ';
-	std::cout << "Please enter a upper case letter to convert from left to right\nand lower to convert from right to left\n\n" << std::endl;
-	std::cout << "[D] Mpg <---> L/100km [d]" << std::endl;
-	std::cout << "[E] Kilograms <---> Stone & Pounds [e]" << std::endl;
-	std::cout << "[F] Celcius <---> Fahrenheit [f]" << std::endl;
-	std::cout << "[Q] Exit [---] Exit [q]" << std::endl;
-	std::cout << "Enter your choice: ";
-	std::cin >> choice;
-	return choice;
 }
