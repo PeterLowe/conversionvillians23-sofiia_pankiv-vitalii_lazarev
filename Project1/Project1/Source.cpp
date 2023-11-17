2695aa8b066f4e3b94f8fe2af5ab7ab07b711b07
(Student  C00301189) Sofiia Pankiv <C00301189@setu.ie>
2023-11-17 10:06:26 +0000

    Code printMenu from A to C

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -1,6 +1,24 @@
 //Sofiia Pankiv
 
+#include <iostream>
+
+char printMenu() // written by Sofiia
+{
+    char choice = ' ';// user input  choice
+    std::cout << "Please enter a upper case letter to convert from left to right\nand lower to convert from right to left\n\n" << std::endl;
+    std::cout << "[A] Degrees---<--->---Radians        [a]" << std::endl;
+    std::cout << "[B] Centimeters-<--->--Feet & Inches  [b]" << std::endl;
+    std::cout << "[C] Litres---<--->---Gallons        [c]" << std::endl;
+
+    std::cout << "[Q] Exit        [---]   Exit           [q]" << std::endl;
+    std::cout << "Enter your Choice : ";
+    std::cin >> choice;
+    return choice;
+}
+
 int main() {
+    char choice;
+    choice = printMenu();
 
 	return 1;
 }
